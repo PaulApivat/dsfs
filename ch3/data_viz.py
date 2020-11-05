fddd7c612b9e56b561e436841eacaca07779ee3c
paulapivat <paul.apivat@gmail.com>
2020-11-05 19:58:33 +0700

    update data_viz.py

--- ch3/data_viz.py
+++ ch3/data_viz.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 from matplotlib import pyplot as plt
+
 years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
 gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
 plt.plot(years, gdp, color="green", marker='o', linestyle='solid')
 plt.title("Nominal GDP")
 plt.ylabel("Billions of $")
 plt.show()
-movies ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
+
 movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
 num_oscars = [5,11,3,8,10]
 plt.bar(range(len(movies)), num_oscars)
@@ -14,9 +15,9 @@ plt.title("My Favorite Movies")
 plt.ylabel("# of Academy Awards")
 plt.xticks(range(len(movies)), movies)
 plt.show()
-grades = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0]
-histogram = Counter(min(grade // 10 * 10, 90) for grade in grades)
+
 from collections import Counter
+grades = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0]
 histogram = Counter(min(grade // 10 * 10, 90) for grade in grades)
 plt.bar([x + 5 for x in histogram.keys()],
         histogram.values(),
@@ -28,15 +29,53 @@ plt.xlabel("Decile")
 plt.ylabel("# of Students")
 plt.title("Distribution of Exam 1 Grades")
 plt.show()
+
+#------------------------------------ Bar Charts ------------------------------------#
+
 mentions = [500, 505]
 years = [2017,2018]
+
+#------------------- misleading y-axis only shows the part above 500 ---------------------#
+
 plt.bar(years, mentions, 0.8)
 plt.xticks(years)
 plt.ylabel("# of times I heard someone say 'data science'")
+
+# if you don't do this, matplotlib will label the x-axis 0, 1
+# then add a +2.013e3 off in the corner 
 plt.ticklabel_format(useOffset=False)
-plt.axis([2016.5, 2018.5, 0, 550])
-plt.title("Not so Huge Anymore")
-plt.show()
+
 plt.axis([2016.5, 2018.5, 499, 506])
 plt.title("Look at the 'Huge' Increase!")
 plt.show()
+
+#---------------------------------- more sensible y-axis ----------------------------------#
+# need to run several lines together
+plt.bar(years, mentions, 0.8)
+plt.xticks(years)
+plt.ylabel("# of times I heard someone say 'data science'")
+
+# if you don't do this, matplotlib will label the x-axis 0, 1
+# then add a +2.013e3 off in the corner 
+plt.ticklabel_format(useOffset=False)
+
+plt.axis([2016.5, 2018.5, 0, 550])
+plt.title("Not so Huge Anymore")
+plt.show()
+
+#------------------------------------ Line Charts ------------------------------------#
+
+variance = [1,2,4,8,16,32,64,128,256]
+bias_squared = [256, 128, 64, 32, 16, 8, 4, 2, 1]
+total_error = [x + y for x,y in zip(variance, bias_squared)]
+xs = [i for i, _ in enumerate(variance)]
+
+plt.plot(xs, variance, 'g-', label='variance')
+plt.plot(xs, bias_squared, 'r-', label='bias^2')
+plt.plot(xs, total_error, 'b:', label='total error')
+plt.legend(loc=9)
+plt.xlabel("model complexity")
+plt.xticks([])
+plt.title("The Bias-Variance Tradeoff")
+plt.show()
+
