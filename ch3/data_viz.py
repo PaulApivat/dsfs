121293228489de6c7dfc1cd86b3950577c3cbf78
paulapivat <paul.apivat@gmail.com>
2020-11-05 20:23:09 +0700

    edit data_viz.py

--- ch3/data_viz.py
+++ ch3/data_viz.py
@@ -6,6 +6,8 @@ gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
 plt.plot(years, gdp, color="green", marker='o', linestyle='solid')
 plt.title("Nominal GDP")
 plt.ylabel("Billions of $")
+plt.xlabel("Years")
+plt.legend("L")
 plt.show()
 
 movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
