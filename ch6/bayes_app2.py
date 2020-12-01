12487eda00fbde77bf92e04f1260c6fd9380b109
paulapivat <paul.apivat@gmail.com>
2020-12-01 14:54:27 +0700

    add bayes_app2.py

---
+++ ch6/bayes_app2.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from random import random, seed
+seed(0)
+pop = 20000000 # 20M people
+counts = {}
+for i in range(pop):
+    has_covid = i % 100 == 0 # update info: 1/1000 have covid, but 1/100 with symptoms have covid
+    if has_covid:
+        tests_positive = True
+        if random() < 0.1: # false negative at 10%
+            tests_positive = False
+    else:
+        tests_positive = False
+        if random() < 0.01: # false positive at 1%
+            tests_positive = True
+    outcome = (has_covid, tests_positive)
+    counts[outcome] = counts.get(outcome, 0) + 1
+    
+for (has_covid, tested_positive), n in counts.items():
+    print('has covid: %6s, tests positive: %6s, count: %d' % (has_covid, tested_positive, n))
+    
+n_positive = counts[(True, True)] + counts[(False, True)]
+print('number of people who tested positive:', n_positive)
+print('probability that a test-positive person with symptoms actually has covid: %.2f' % (100.0 * counts[(True, True)] / n_positive),)
