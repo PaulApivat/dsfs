09bbba248d0fb2097be6811ea7c7a21c0233c8d6
paulapivat <paul.apivat@gmail.com>
2020-12-02 13:04:42 +0700

    add bayes_app_highest2.py

---
+++ ch6/bayes_app_highest2.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+from random import random, seed
+seed(0)
+pop = 20000000 # 20M people
+counts = {}
+for i in range(pop):
+    has_covid = i % 100 == 0 # have same prior as bayes_app2.py (1 in 100 people have covid, but different false positives/negatives)
+    # assuming that every person gets tested regardless of any symptoms
+    if has_covid:
+        tests_positive = True
+        if random() < 0.2: # random coin flip creates 20% false negative
+            tests_positive = False
+    else:
+        tests_positive = False
+        if random() < 0.1: # random coin flip creates 10% false positive
+            tests_positive = True
+    outcome = (has_covid, tests_positive)
+    counts[outcome] = counts.get(outcome, 0) + 1
+    
+for (has_covid, tested_positive), n in counts.items():
+    print('has covid: %6s, test positive: %6s, count: %d' % (has_covid, tested_positive, n))
+    
+n_positive = counts[(True, True)] + counts[(False, True)]
+print('number of people who tested positive:', n_positive)
+print('probability that a test-positive person actually has covid: %.2f' % (100.0 * counts[(True, True)] / n_positive),)
