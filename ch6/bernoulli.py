415fbcd83be9fc891a45da97bed4924332fdb62d
paulapivat <paul.apivat@gmail.com>
2020-12-08 08:52:46 +0700

    edit bernoulli and pmf

--- ch6/bernoulli.py
+++ ch6/bernoulli.py
@@ -5,27 +5,8 @@
 from random import random, seed
 
 seed(0)
-pop = 1000
+pop = 10000
 
-counts = {}
-
-for i in range(pop):
-    has_covid = i % 10 == 0
-    if has_covid:
-        tests_positive = True
-        if random() < 0.1:
-            tests_positive = False
-    else:
-        test_positive = False
-        if random() < 0.01:
-            tests_positive = True
-    outcome = (has_covid, tests_positive)
-    counts[outcome] = counts.get(outcome, 0) + 1
-
-# should roughly be TT: 9, FT: 81, TF: 1, FF: 9
-for (has_covid, tested_positive), n in counts.items():
-    print('has covid: %6s, tests positive: %6s, count: %d' %
-          (has_covid, tested_positive, n))
 
 # Testing bernoulli_trial
 # bernoulli_trial(0.10) - 1/10 having disease
@@ -37,7 +18,7 @@ def bernoulli_trial(p: float) -> int:
 
 
 counts = {}
-p = 0.9
+p = 0.5
 
 for i in range(pop):
     has_covid = bernoulli_trial(p)
@@ -55,3 +36,8 @@ for i in range(pop):
 for (has_covid, tested_positive), n in counts.items():
     print('Bernoulli_Trial: has covid: %6s, tests positive: %6s, count: %d' %
           (has_covid, tested_positive, n))
+
+n_positive = counts[(True, True)] + counts[(False, True)]
+print('Bernoulli_Trial: Number of People who Tested Positive:', n_positive)
+print('Bernoulli_Trial: Probability that a test-positive person actually has covid: %.2f' %
+      (100.0 * counts[(True, True)] / n_positive), )
--- ch6/pmf.py
+++ ch6/pmf.py
@@ -7,26 +7,26 @@ seed(0)
 pop = 1000  # 1000 people
 counts = {}
 for i in range(pop):
-    has_covid = i % 10 == 0  # one in 10 people have covid
+    has_disease = i % 10 == 0  # one in 10 people have disease
     # assuming that every person gets tested regardless of any symptoms
-    if has_covid:
-        tests_positive = True
-        if random() < 0.2:  # random coin flip creates 20% false negative
-            tests_positive = False
+    if has_disease:
+        tests_positive = True       # True Positive  95%
+        if random() < 0.05:
+            tests_positive = False  # False Negative 5%
     else:
-        tests_positive = False
-        if random() < 0.1:  # random coin flip creates 10% false positive
-            tests_positive = True
-    outcome = (has_covid, tests_positive)
+        tests_positive = False      # True Negative  90%
+        if random() < 0.1:
+            tests_positive = True   # False Positive 10%
+    outcome = (has_disease, tests_positive)
     counts[outcome] = counts.get(outcome, 0) + 1
 
-for (has_covid, tested_positive), n in counts.items():
-    print('has covid: %6s, test positive: %6s, count: %d' %
-          (has_covid, tested_positive, n))
+for (has_disease, tested_positive), n in counts.items():
+    print('Has Disease: %6s, Test Positive: %6s, count: %d' %
+          (has_disease, tested_positive, n))
 
 n_positive = counts[(True, True)] + counts[(False, True)]
-print('number of people who tested positive:', n_positive)
-print('probability that a test-positive person actually has covid: %.2f' %
+print('Number of people who tested positive:', n_positive)
+print('Probability that a test-positive person actually has disease: %.2f' %
       (100.0 * counts[(True, True)] / n_positive),)
 
 
@@ -37,9 +37,11 @@ df = df.rename(columns={0: 'item_counts'})
 df['probability'] = df['item_counts']/1000
 df['item2'] = ['TT', 'FF', 'FT', 'TF']
 plt.bar(df['item2'], df['probability'])
+plt.title("Probability Mass Function")
 plt.show()
 
 df['cumsum'] = df['item_counts'].cumsum()
 df['probability2'] = df['cumsum']/1000
 plt.bar(df['item2'], df['probability2'])
+plt.title("Cumulative Distribution Function")
 plt.show()
