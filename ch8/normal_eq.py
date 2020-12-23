f4e4b11b5f4bd00e2cbc89efa3740fb8770d0451
paulapivat <paul.apivat@gmail.com>
2020-12-23 15:08:28 +0700

    add to normal_eq.py

--- ch8/normal_eq.py
+++ ch8/normal_eq.py
@@ -19,3 +19,36 @@ Y = df['Writing'].values
 ax = plt.axes(projection='3d')
 ax.scatter(X1, X2, Y, c=Y, cmap='viridis', linewidth=0.5)
 plt.show()
+
+# generate training and testing data
+X0 = np.ones(len(X1))
+X = np.array([X0, X1, X2]).T
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.05)
+print("X_train shape:", x_train.shape, "\nY_train shape:", y_train.shape)
+print("X_test shape:", x_test.shape, "\nY_test shape:", y_test.shape)
+
+# Gradient Descent
+Q = np.zeros(3)
+n = len(X1)
+
+
+def cost_function(X, Y, Q):
+    return np.sum(((X.dot(Q)-Y)**2)/(2*n))
+
+
+def gradient_descent(X, Y, Q, epochs, alpha):
+    cost_history = np.zeros(epochs)
+    for i in range(epochs):
+        pred = X.dot(Q)
+        loss = pred-Y
+        gradient = X.T.dot(loss)/n
+        Q = Q-gradient*alpha
+        cost_history[i] = cost_function(X, Y, Q)
+    return cost_history, Q
+
+
+start = time.time()
+cost_his, parameters = gradient_descent(
+    x_train, y_train.flatten(), Q, 1000, 0.0001)
+end = time.time()
+print(end - start)
