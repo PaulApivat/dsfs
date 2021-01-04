49eee9b5f92d763ababa9a9c7a36925c62651929
paulapivat <paul.apivat@gmail.com>
2021-01-04 20:29:28 +0700

    add own github exploration to unauth_api.py

--- ch9/unauth_api.py
+++ ch9/unauth_api.py
@@ -2,6 +2,8 @@
 # APIs save you trouble from having to scrap your data
 # HTTP is a protocol for transfering text data, so all data through a Web API is serialized into string (text) format
 
+from dateutil.parser import parse
+from collections import Counter
 import requests
 import json
 
@@ -31,3 +33,19 @@ type(repos)  # list
 # loop through repos, grab all "url" keys
 for repo in repos:
     print(repo['url'])
+
+##### ------ Access GitHub public API ------######
+
+
+dates = [parse(repo['created_at']) for repo in repos]
+month_counts = Counter(date.month for date in dates)
+weekday_counts = Counter(date.weekday() for date in dates)
+last_5_repositories = sorted(repos,
+                             key=lambda r: r['pushed_at'],
+                             reverse=True)[:5]
+
+# ['Python', 'HTML', 'R', 'R', 'R']
+last_5_languages = [repo['language'] for repo in last_5_repositories]
+
+last_5_repositories
+print(dates)
